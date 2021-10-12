a8be41bd55385151bca1cb60c2a0a3188eb9e126
Fuyao Zhao <zfy0701@gmail.com>
2021-10-12 09:40:31 -0700

    Add docker files for soteria environment

---
+++ soteria-base/release.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker build -t greencorelab/soteria-base:0.1.0 .
+docker push greencorelab/soteria-base:0.1.0
---
+++ soteria/release.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker build -t greencorelab/soteria:0.1.0 .
+docker push greencorelab/soteria:0.1.0
