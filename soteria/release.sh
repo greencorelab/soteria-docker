f82e6631dfe14cc02ade81ebb8fb104f09b75cad
coderrect <47907149+coderrect@users.noreply.github.com>
2022-02-03 15:14:50 -0600

    soteria:0.1.3

--- soteria/release.sh
+++ soteria/release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t greencorelab/soteria:0.1.0 .
-docker push greencorelab/soteria:0.1.0
+docker build -t greencorelab/soteria:0.1.3 .
+docker push greencorelab/soteria:0.1.3
